7f9f2b5bcfe2740d25d7378598ad4f469a099eed
KONATA_CG <11911409@mail.sustech.edu.cn>
2021-10-11 20:52:24 +0800

    ai

--- CS303--AI/Project/project1/main.py
+++ CS303--AI/Project/project1/main.py
@@ -8,6 +8,28 @@ COLOR_NONE = 0
 random.seed(0)
 
 
+class Neighbor:
+    position = ()
+    distance = 0
+
+    def __init__(self, cur_pos, neighbor_pos):
+        self.position = neighbor_pos
+        self.measure_distance(cur_pos)
+
+    # calculate how many rival's chess is between you and your neighbor
+    def measure_distance(self, cur_pos):
+        if self.position[0] == cur_pos[0]:
+            if self.position[1] >= cur_pos[1]:
+                self.distance = self.position[1] - cur_pos[1] - 1
+            else:
+                self.distance = cur_pos[1] - self.position[1] - 1
+        else:
+            if self.position[0] >= cur_pos[0]:
+                self.distance = self.position[0] - cur_pos[0] - 1
+            else:
+                self.distance = cur_pos[0] - self.position[0] - 1
+
+
 # don't change the class name
 class AI(object):
     # chessboard_size, color, time_out passed from agent
@@ -21,142 +43,145 @@ class AI(object):
         # decision
         self.candidate_list = []
 
+    # The input is current chessboard.
+    def go(self, chessboard):
+        # Clear candidate_list, must do this step
+        self.candidate_list.clear()
 
-# The input is current chessboard.
-def go(self, chessboard):
-    # Clear candidate_list, must do this step
-    self.candidate_list.clear()
+        # ==================================================================
+        # Write your algorithm here
+        # Here is the simplest sample:Random decision
+        # row : idx[0], column : idx[1]
+        idx = np.where(chessboard == COLOR_NONE)
+        idx = list(zip(idx[0], idx[1]))
 
-    # ==================================================================
-    # Write your algorithm here
-    # Here is the simplest sample:Random decision
-    # row : idx[0], column : idx[1]
-    idx = np.where(chessboard == COLOR_NONE)
-    idx = list(zip(idx[0], idx[1]))
+        # ==============Find new pos========================================
+        # Make sure that the position of your decision in chess board is empty.
+        # If not, the system will return error.
+        # Add your decision into candidate_list, Records the chess board
+        # You need add all the positions which is valid
+        # candidate_list example: [(3,3),(4,4)]
+        # You need append your decision at the end of the candidate_list,
+        # we will choice the last element of the candidate_list as the position you choose
+        # If there is no valid position, you must return an empty list.
+        start = time.time()
+        neighbor_dict = {}
+        for pos in idx:
+            neighbor_list = []
+            # check all positions in the same row / column / diagonal
+            # find the nearest position which is in the same color, add it into neighbor list
+            # column_down
+            if pos[0] < self.chessboard_size - 1:
+                for row_idx in [pos[0] + 1, self.chessboard_size - 1]:
+                    if chessboard[row_idx][pos[1]] == self.color:
+                        neighbor_list.append(Neighbor(pos, (row_idx, pos[1])))
+                        break
+                    elif chessboard[row_idx][pos[1]] != COLOR_NONE:
+                        continue
+                    else:
+                        break
+            # column_up
+            if pos[0] > 0:
+                for row_idx in [0, pos[0] - 1]:
+                    if chessboard[(pos[0] - 1) - row_idx][pos[1]] == self.color:
+                        neighbor_list.append(Neighbor(pos, ((pos[0] - 1) - row_idx, pos[1])))
+                        break
+                    elif chessboard[(pos[0] - 1) - row_idx][pos[1]] != COLOR_NONE:
+                        continue
+                    else:
+                        break
+            # row_left
+            if pos[1] < self.chessboard_size - 1:
+                for col_idx in [pos[1] + 1, self.chessboard_size - 1]:
+                    if chessboard[pos[0]][col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (pos[0], col_idx)))
+                        break
+                    elif chessboard[pos[0]][col_idx] != COLOR_NONE:
+                        continue
+                    else:
+                        break
+            # row_right
+            if pos[1] > 0:
+                for col_idx in [0, pos[1] - 1]:
+                    if chessboard[pos[0]][(pos[1] - 1) - col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (pos[0], (pos[1] - 1) - col_idx)))
+                        break
+                    elif chessboard[pos[0]][(pos[1] - 1) - col_idx] != COLOR_NONE:
+                        continue
+                    else:
+                        break
+            # diagonal_right_down
+            if pos[1] < self.chessboard_size - 1 and pos[0] < self.chessboard_size - 1:
+                row_idx = pos[0] + 1
+                col_idx = pos[1] + 1
+                while row_idx <= self.chessboard_size - 1 and col_idx <= self.chessboard_size - 1:
+                    if chessboard[row_idx][col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (row_idx, col_idx)))
+                        break
+                    elif chessboard[row_idx][col_idx] != COLOR_NONE:
+                        row_idx = row_idx + 1
+                        col_idx = col_idx + 1
+                        continue
+                    else:
+                        break
+            # diagonal_right_up
+            if pos[1] < self.chessboard_size - 1 and pos[0] > 0:
+                row_idx = pos[0] - 1
+                col_idx = pos[1] + 1
+                while row_idx >= 0 and col_idx <= self.chessboard_size - 1:
+                    if chessboard[row_idx][col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (row_idx, col_idx)))
+                        break
+                    elif chessboard[row_idx][col_idx] != COLOR_NONE:
+                        row_idx = row_idx - 1
+                        col_idx = col_idx + 1
+                        continue
+                    else:
+                        break
+            # diagonal_left_up
+            if pos[1] > 0 and pos[0] > 0:
+                row_idx = pos[0] - 1
+                col_idx = pos[1] - 1
+                while row_idx >= 0 and col_idx >= 0:
+                    if chessboard[row_idx][col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (row_idx, col_idx)))
+                        break
+                    elif chessboard[row_idx][col_idx] != COLOR_NONE:
+                        row_idx = row_idx - 1
+                        col_idx = col_idx - 1
+                        continue
+                    else:
+                        break
+            # diagonal_left_down
+            if pos[1] > 0 and pos[0] < self.chessboard_size - 1:
+                row_idx = pos[0] + 1
+                col_idx = pos[1] - 1
+                while row_idx <= self.chessboard_size - 1 and col_idx >= 0:
+                    if chessboard[row_idx][col_idx] == self.color:
+                        neighbor_list.append(Neighbor(pos, (row_idx, col_idx)))
+                        break
+                    elif chessboard[row_idx][col_idx] != COLOR_NONE:
+                        row_idx = row_idx + 1
+                        col_idx = col_idx - 1
+                        continue
+                    else:
+                        break
 
-    # ==============Find new pos========================================
-    # Make sure that the position of your decision in chess board is empty.
-    # If not, the system will return error.
-    # Add your decision into candidate_list, Records the chess board
-    # You need add all the positions which is valid
-    # candidate_list example: [(3,3),(4,4)]
-    # You need append your decision at the end of the candidate_list,
-    # we will choice the last element of the candidate_list as the position you choose
-    # If there is no valid position, you must return an empty list.
-    neighbor = {}
-    for pos in idx:
-        # check have neighbor in opposite color
-        if chessboard[pos[0] + 1][pos[1] + 1] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] + 1][pos[1] - 1] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] + 1][pos[1]] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] - 1][pos[1] + 1] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] - 1][pos[1] - 1] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] - 1][pos[1]] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] + 1][pos[1] + 1] == (self.color or COLOR_NONE) and \
-                chessboard[pos[0] + 1][pos[1] - 1] == (self.color or COLOR_NONE):
-            continue
+            neighbor_list.sort(key=lambda x: x.distance)
+            neighbor_dict[pos] = neighbor_list
 
-        # check all positions in the same row / column / diagonal
-        # find the nearest position which is in the same color, add it into neighbor list
-        neighbor[pos] = []
-        # column_down
-        if pos[0] < self.chessboard_size - 1:
-            for row_idx in [pos[0] + 1, self.chessboard_size - 1]:
-                if chessboard[row_idx][pos[1]] == self.color:
-                    neighbor[pos].append((row_idx, pos[1]))
-                    break
-                elif chessboard[row_idx][pos[1]] != COLOR_NONE:
-                    continue
-                else:
-                    break
+            # check pos valid
+            if len(neighbor_dict[pos]) != 0:
+                self.candidate_list.append(pos)
 
-        # column_up
-        if pos[0] > 0:
-            for row_idx in [0, pos[0] - 1]:
-                if chessboard[(pos[0] - 1) - row_idx][pos[1]] == self.color:
-                    neighbor[pos].append(((pos[0] - 1) - row_idx, pos[1]))
-                    break
-                elif chessboard[(pos[0] - 1) - row_idx][pos[1]] != COLOR_NONE:
-                    continue
-                else:
-                    break
-        # row_left
-        if pos[1] < self.chessboard_size - 1:
-            for col_idx in [pos[1] + 1, self.chessboard_size - 1]:
-                if chessboard[pos[0]][col_idx] == self.color:
-                    neighbor[pos].append((pos[0])(col_idx))
-                    break
-                elif chessboard[pos[0]][col_idx] != COLOR_NONE:
-                    continue
-                else:
-                    break
-        # row_right
-        if pos[1] > 0:
-            for col_idx in [0, pos[1] - 1]:
-                if chessboard[pos[0]][(pos[1] - 1) - col_idx] == self.color:
-                    neighbor[pos].append((pos[0])((pos[1] - 1) - col_idx))
-                    break
-                elif chessboard[pos[0]][(pos[1] - 1) - col_idx] != COLOR_NONE:
-                    continue
-                else:
-                    break
-        # diagonal_right_down
-        if pos[1] < self.chessboard_size - 1 and pos[0] < self.chessboard_size - 1:
-            row_idx = pos[0] + 1
-            col_idx = pos[1] + 1
-            while row_idx <= self.chessboard_size - 1 and col_idx <= self.chessboard_size - 1:
-                if chessboard[row_idx][col_idx] == self.color:
-                    neighbor[pos].append((row_idx, col_idx))
-                    break
-                elif chessboard[row_idx][col_idx] != COLOR_NONE:
-                    row_idx = row_idx + 1
-                    col_idx = col_idx + 1
-                    continue
-                else:
-                    break
-        # diagonal_right_up
-        if pos[1] < self.chessboard_size - 1 and pos[0] > 0:
-            row_idx = pos[0] - 1
-            col_idx = pos[1] + 1
-            while row_idx >= 0 and col_idx <= self.chessboard_size - 1:
-                if chessboard[row_idx][col_idx] == self.color:
-                    neighbor[pos].append((row_idx, col_idx))
-                    break
-                elif chessboard[row_idx][col_idx] != COLOR_NONE:
-                    row_idx = row_idx - 1
-                    col_idx = col_idx + 1
-                    continue
-                else:
-                    break
-        # diagonal_left_up
-        if pos[1] > 0 and pos[0] > 0:
-            row_idx = pos[0] - 1
-            col_idx = pos[1] - 1
-            while row_idx >= 0 and col_idx >= 0:
-                if chessboard[row_idx][col_idx] == self.color:
-                    neighbor[pos].append((row_idx, col_idx))
-                    break
-                elif chessboard[row_idx][col_idx] != COLOR_NONE:
-                    row_idx = row_idx - 1
-                    col_idx = col_idx - 1
-                    continue
-                else:
-                    break
-        # diagonal_left_down
-        if pos[1] > 0 and pos[0] < self.chessboard_size - 1:
-            row_idx = pos[0] + 1
-            col_idx = pos[1] - 1
-            while row_idx <= self.chessboard_size - 1 and col_idx >= 0:
-                if chessboard[row_idx][col_idx] == self.color:
-                    neighbor[pos].append((row_idx, col_idx))
-                    break
-                elif chessboard[row_idx][col_idx] != COLOR_NONE:
-                    row_idx = row_idx + 1
-                    col_idx = col_idx - 1
-                    continue
-                else:
-                    break
+        pre_candidate = []
+        for candidate_pos in self.candidate_list:
+            if len(neighbor_dict[candidate_pos]) != 0:
+                pre_candidate.append(neighbor_dict[candidate_pos][0])
+        pre_candidate.sort(key=lambda x: x.distance)
+        self.candidate_list.append(pre_candidate[0])
 
-        # check pos valid
-        if len(neighbor[pos]) != 0:
-            self.candidate_list.append(pos)
+        runtime = (time.time() - start)
+
+    # line 170, in go neighbor_dict[pos] = (neighbor_list, neighbor_list[0]) IndexError: list index out of range
+    # TypeError: 'Neighbor' object is not iterable
