433da18cf99ae45eeb38bcc09caefd8b1eeba2ba
Konata-CG <80034306+Konata-CG@users.noreply.github.com>
2021-10-28 21:23:05 +0800

    ai_pro

--- CS303--AI/Project/project1/main.py
+++ CS303--AI/Project/project1/main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
+import math
 import time
+
+infinity = math.inf
 
 COLOR_BLACK = -1
 COLOR_WHITE = 1
@@ -10,6 +13,7 @@ random.seed(0)
 
 class Neighbor:
     position = ()
+
     # distance = 0
 
     def __init__(self, cur_pos, neighbor_pos):
@@ -44,138 +48,139 @@ class AI(object):
         self.candidate_list = []
 
     # give a position on chessboard, return places can be placed chess
-    def trace_neighbor(self, chessboard, pos):
+
+    def trace_neighbor(self, cur_state, target_pos, cur_player_color):
         # check all positions in the same row / column / diagonal
         # find the nearest position which is in the same color, add it into neighbor list
         neighbor_list = []
 
         # column_down
-        target_row = pos[0] + 1
-        target_col = pos[1]
+        target_row = target_pos[0] + 1
+        target_col = target_pos[1]
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row + 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # column_up
-        target_row = pos[0] - 1
-        target_col = pos[1]
+        target_row = target_pos[0] - 1
+        target_col = target_pos[1]
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row - 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # row_right
-        target_row = pos[0]
-        target_col = pos[1] + 1
+        target_row = target_pos[0]
+        target_col = target_pos[1] + 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_col = target_col + 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # row_left
-        target_row = pos[0]
-        target_col = pos[1] - 1
+        target_row = target_pos[0]
+        target_col = target_pos[1] - 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_col = target_col - 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # diagonal_right_down
-        target_row = pos[0] + 1
-        target_col = pos[1] + 1
+        target_row = target_pos[0] + 1
+        target_col = target_pos[1] + 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row + 1
                 target_col = target_col + 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # diagonal_right_up
-        target_row = pos[0] - 1
-        target_col = pos[1] + 1
+        target_row = target_pos[0] - 1
+        target_col = target_pos[1] + 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row - 1
                 target_col = target_col + 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # diagonal_left_down
-        target_row = pos[0] + 1
-        target_col = pos[1] - 1
+        target_row = target_pos[0] + 1
+        target_col = target_pos[1] - 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row + 1
                 target_col = target_col - 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
                 break
 
         # diagonal_right_up
-        target_row = pos[0] - 1
-        target_col = pos[1] - 1
+        target_row = target_pos[0] - 1
+        target_col = target_pos[1] - 1
         pass_enemy_flag = -1
         while (self.chessboard_size - 1 >= target_row >= 0) and (self.chessboard_size - 1 >= target_col >= 0):
-            if chessboard[target_row][target_col] == COLOR_NONE:
+            if cur_state[target_row][target_col] == COLOR_NONE:
                 break
-            elif chessboard[target_row][target_col] != self.color:
+            elif cur_state[target_row][target_col] != cur_player_color:
                 target_row = target_row - 1
                 target_col = target_col - 1
                 pass_enemy_flag = 1
-            elif chessboard[target_row][target_col] == self.color and pass_enemy_flag == 1:
+            elif cur_state[target_row][target_col] == cur_player_color and pass_enemy_flag == 1:
                 neighbor_list.append((target_row, target_col))
                 break
             else:
@@ -183,40 +188,69 @@ class AI(object):
 
         return neighbor_list
 
-    def find_candidate(self, neighbor_dict, chessboard, idx):
+    def search_candidate(self, chessboard, cur_player_color):
+        # row : idx[0], column : idx[1]
+        # idx : all empty space in chess board
+        idx = np.where(chessboard == COLOR_NONE)
+        idx = list(zip(idx[0], idx[1]))
+        neighbor_dict = {}
+        candidate_list = []
         for pos in idx:
-            neighbor_dict[pos] = self.trace_neighbor(chessboard, pos)
-
-            # check pos valid
+            neighbor_dict[pos] = self.trace_neighbor(chessboard, pos, cur_player_color)
             if len(neighbor_dict[pos]) != 0:
-                self.candidate_list.append(pos)
+                candidate_list.append(pos)
+        return neighbor_dict, candidate_list
+
+    def terminal_test(self, cur_chessboard_state):
+        # 双方都没地方落子游戏结束
+        _, candidate_list_1 = self.search_candidate(cur_chessboard_state, self.color)
+        _, candidate_list_2 = self.search_candidate(cur_chessboard_state, -self.color)
+        return len(candidate_list_1) == 0 and len(candidate_list_2) == 0
+
+    def evaluate(self, cur_state, cur_player):
+        return 0
+
+    def possible_actions(self, cur_chessboard_state, cur_player_color):
+        possible_next_state = []
+        _, candidate_list = self.search_candidate(cur_chessboard_state, cur_player_color)
+        for candidate_pos in candidate_list:
+            new_chessboard_state = cur_chessboard_state
+            new_chessboard_state[candidate_pos[0]][candidate_pos[1]] = cur_player_color
+            possible_next_state.append(new_chessboard_state)
+        return possible_next_state
+
+    def alpha_beta(self, chessboard):
+
+        def max_value(cur_state):
+            self.search_candidate()
+            if self.terminal_test(cur_state):
+                return self.evaluate(cur_state, self.color)
+            value = -infinity
+            for next_state in self.possible_actions(cur_state, self.color):
+                value = max(value, min_value(next_state))
+            return value
+
+        def min_value(cur_state):
+            if self.terminal_test(cur_state):
+                return self.evaluate(cur_state, -self.color)
+            value = infinity
+            for next_state in self.possible_actions(cur_state, -self.color):
+                value = min(value, max_value(next_state))
+            return value
+
+        return max_value(chessboard)
 
     # The input is current chessboard.
     def go(self, chessboard):
         # Clear candidate_list, must do this step
         self.candidate_list.clear()
 
-        # ==================================================================
-        # Write your algorithm here
-        # Here is the simplest sample:Random decision
-        # row : idx[0], column : idx[1]
-        # idx : all empty space in chess board
-        idx = np.where(chessboard == COLOR_NONE)
-        idx = list(zip(idx[0], idx[1]))
-
-        # ==============Find new pos========================================
-        # Make sure that the position of your decision in chess board is empty.
-        # If not, the system will return error.
-        # Add your decision into candidate_list, Records the chess board
-        # You need add all the positions which is valid
-        # candidate_list example: [(3,3),(4,4)]
-        # You need append your decision at the end of the candidate_list,
-        # we will choice the last element of the candidate_list as the position you choose
-        # If there is no valid position, you must return an empty list.
+        # candidate_list example: [pos1, pos2, ...]
+        # pos example:  (row_number, col_number)
         start = time.time()
-        neighbor_dict = {}      # 维护一个字典存放： 位置坐标 -> 附近的邻居列表
 
         # 遍历棋盘，进行基本的位置搜寻和信息提取
-        self.find_candidate(neighbor_dict, chessboard, idx)
+        # 维护一个字典存放： 位置坐标 -> 附近的邻居列表
+        neighbor_dict, self.candidate_list = self.search_candidate(chessboard, self.color)
 
         runtime = (time.time() - start)
